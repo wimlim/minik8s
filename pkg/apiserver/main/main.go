986e81447950d9ff344e2b777e37b99c763c32c0
gxj2003 <2061389208@qq.com>
2024-04-22 19:35:01 +0800

    高肖健:响应http请求完成

--- pkg/apiserver/main/main.go
+++ pkg/apiserver/main/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import(
+	"fmt"
+	"minik8s/pkg/apiserver/app/server"
+	"minik8s/pkg/config"
+)
 
 func main() {
-	fmt.Println("scheduler")
+	fmt.Println("server start")
+	s := server.NewServer(config.ServerDefaultListenIp, config.ServerDefaultPort)
+	s = server.SetServer(s)
+	s.Run()
 }
