1df5dfc1571c79cbf38e5988cae6903133b903d5
gxj2003 <2061389208@qq.com>
2024-05-06 15:14:26 +0800

    etcd fix

--- pkg/etcd/etcdURL.go
+++ pkg/etcd/etcdURL.go
@@ -2,7 +2,7 @@ package etcd
 
 
 const(
-	PATH_EtcdNodes = "/registry/nodes/"
-	PATH_EtcdPods = "/registry/pods/"
-	PATH_EtcdServices = "/registry/services/"
+	PATH_EtcdNodes = "/registry/nodes"
+	PATH_EtcdPods = "/registry/pods"
+	PATH_EtcdServices = "/registry/services"
 )
