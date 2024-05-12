fbce218b9263371a164a035c97622fbf68db44de
DragonKnight-03 <2578103832@qq.com>
2024-05-12 19:22:22 +0800

    pod的create和Delete

--- pkg/kubelet/app/kubelet.go
+++ pkg/kubelet/app/kubelet.go
@@ -1,11 +1,13 @@
 package app
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"minik8s/pkg/apiobj"
 	"minik8s/pkg/kubelet/app/runtime"
 	"minik8s/pkg/message"
+	"os"
 
 	"github.com/streadway/amqp"
 )
@@ -15,10 +17,22 @@ func msgHandler(d amqp.Delivery) {
 	var msg message.Message
 	json.Unmarshal(d.Body, &msg)
 	fmt.Println(msg.Name)
-
 	var pod apiobj.Pod
 	json.Unmarshal([]byte(msg.Content), &pod)
 	runtime.CreatePod(&pod)
+	reader := bufio.NewReader(os.Stdin)
+
+	// 读取回车
+	fmt.Println("\nPod已创建\n按下回车继续...")
+	_, err := reader.ReadBytes('\n')
+	if err != nil {
+		fmt.Println("读取回车时出错：", err)
+		return
+	}
+
+	fmt.Println("继续执行下一步操作：删除Pod...")
+
+	runtime.DeletePod(&pod)
 	fmt.Println(pod.MetaData.Name)
 }
 
--- pkg/kubelet/app/runtime/runtime_test.go
+++ pkg/kubelet/app/runtime/runtime_test.go
@@ -2,8 +2,30 @@ package runtime
 
 import (
 	"fmt"
+	"io/ioutil"
+	"minik8s/pkg/apiobj"
+	"minik8s/pkg/kubelet/app/runtime/container"
+	"minik8s/pkg/minik8sTypes"
 	"testing"
+
+	"github.com/docker/docker/api/types/filters"
+	"gopkg.in/yaml.v3"
 )
+
+func getPodFromYaml(filePath string) (*apiobj.Pod, error) {
+	yamlFile, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		fmt.Printf("读取 YAML 文件失败：%v", err)
+		return nil, err
+	}
+	var pod apiobj.Pod
+	err = yaml.Unmarshal(yamlFile, &pod)
+	if err != nil {
+		fmt.Printf("解析 YAML 文件失败：%v", err)
+		return nil, err
+	}
+	return &pod, nil
+}
 
 func TestFindAvailablePort(t *testing.T) {
 	pausePortSet := map[string]struct{}{}
@@ -22,14 +44,35 @@ func TestFindAvailablePort(t *testing.T) {
 	}
 }
 
-// pauseContainerConfig, err := parsePauseContainerConfig(pod)
-// if err != nil {
-// 	fmt.Println("Error:", err)
-// 	return "", err
-// }
-// jsonData, err := json.MarshalIndent((*pauseContainerConfig), "", "    ")
-// if err != nil {
-// 	fmt.Println("Error:", err)
-// 	return "", err
-// }
-// fmt.Println("\nJSON:\n" + string(jsonData))
+func TestGetPodFromYaml(t *testing.T) {
+	pod, _ := getPodFromYaml("pod1.yaml")
+	fmt.Printf("Pod 名称: %s\n", pod.MetaData.Name)
+	fmt.Println("容器信息:")
+	for _, container := range pod.Spec.Containers {
+		fmt.Printf("- 名称: %s, 镜像: %s\n", container.Name, container.Image)
+	}
+}
+
+func TestRemoveAllCommonContainer(t *testing.T) {
+	pod, _ := getPodFromYaml("pod1.yaml")
+	filterArgs := filters.NewArgs()
+	filterArgs.Add(minik8sTypes.Container_Filter_Label, minik8sTypes.Container_Label_PodName+"="+pod.MetaData.Name)
+	for labelKey, labelValue := range pod.MetaData.Labels {
+		filterArgs.Add(minik8sTypes.Container_Filter_Label, labelKey+"="+labelValue)
+	}
+	commonContainers, _ := container.ListContainerWithFilters(filterArgs)
+	for _, ctn := range commonContainers {
+		fmt.Println(ctn.ID + "\t" + ctn.Image + "\t" + ctn.Names[0])
+	}
+	errMsg := ""
+	for _, commonContainer := range commonContainers {
+		_, err := container.RemoveContainer(commonContainer.ID)
+		if err != nil {
+			errMsg += "RemoveContainer error in container_" + commonContainer.ID + ":\n" + err.Error() + "\n"
+		}
+	}
+	if errMsg != "" {
+		return
+	}
+	return
+}
