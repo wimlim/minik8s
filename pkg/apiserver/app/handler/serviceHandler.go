3c2d7b9d6c9ed5af2d0fe1ed6c100e551df2d2d8
gxj2003 <2061389208@qq.com>
2024-05-13 20:44:11 +0800

    add pod3.yaml

--- pkg/apiserver/app/handler/serviceHandler.go
+++ pkg/apiserver/app/handler/serviceHandler.go
@@ -43,8 +43,9 @@ func GetAllServices(c *gin.Context) {
 
 func GetService(c *gin.Context) {
 	// get service
+	namespace := c.Param("namespace")
 	name := c.Param("name")
-	key := fmt.Sprintf(etcd.PATH_EtcdServices+"/%s", name)
+	key := fmt.Sprintf(etcd.PATH_EtcdServices+"/%s/%s", namespace, name)
 	res, err := etcd.EtcdKV.Get(key)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"get": "fail"})
