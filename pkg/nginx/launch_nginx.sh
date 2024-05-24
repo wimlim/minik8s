2c402e67369735bdc4feb34f3cf2761656471809
wimlim <wimlim33@gmail.com>
2024-05-24 20:53:58 +0800

    nginx setting

--- pkg/nginx/launch_nginx.sh
+++ pkg/nginx/launch_nginx.sh
@@ -1,1 +1,2 @@
-docker run -d --name my-nginx-container -p 80:80 -v /root/minik8s/pkg/nginx:/etc/nginx/conf.d nginx
+docker run -d --privileged --name my-nginx-container -p 80:80 -v /root/minik8s/pkg/nginx:/etc/nginx/conf.d nginx
+docker exec my-nginx-container apt-get update && apt-get install -y ipvsadm
